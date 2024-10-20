6a8f263cdae482f99a3d5f9fd691e8cd8e0fbac1
serbantimofte <serban-timofte@outlook.com>
2024-10-20 23:02:10 +0300

    Functia calcul valori

--- 2_Seminar/pythonProject/functii.py
+++ 2_Seminar/pythonProject/functii.py
@@ -1,12 +1,93 @@
-def calcul_indicatori(tabel, variabile_numerice):
-    # Medie
-    # Standard Deviation
-    # Covarianta
+import csv
+import math
 
-    # Verificam daca tabelul nostru este dictionar
+
+def csv_to_dic(filename):
+    dict = {}
+    with open(filename, 'r') as csvFile:
+        reader = csv.reader(csvFile)
+        next(reader)
+        for row in reader:
+            siruta = int(row[0])
+            localitate = row[1]
+            judet = row[2]
+            abs_liceal = float(row[3])
+            abs_postlic = float(row[4])
+            abs_primar_gimn = float(row[5])
+            abs_profes = float(row[6])
+            abs_tehnic = float(row[7])
+            abs_univ = float(row[8])
+            pop_liceal = float(row[9])
+            pop_profess = float(row[10])
+            pop_primar_gimn = float(row[11])
+            pop_univ = float(row[12])
+
+            # Stocăm datele într-un tuplu
+            dict[siruta] = (localitate, judet, abs_liceal, abs_postlic, abs_primar_gimn, abs_profes,
+                             abs_tehnic, abs_univ, pop_liceal, pop_profess, pop_primar_gimn, pop_univ)
+    return dict
+
+
+def calcul_indicatori(tabel_date, denumiri_variabile_numerice):
+    """
+    Calculează media, deviația standard și coeficientul de variație
+    pentru variabilele specificate în tabel. Indicatori calculati pe coloane
+
+    :param tabel_date: Dicționar cu datele (Siruta ca cheie).
+    :param denumiri_variabile: Lista cu denumirile variabilelor numerice.
+    :return: Un dicționar cu statistici pentru fiecare variabilă.
+    """
+    rezultate_statistici = {}
+
     try:
-        assert isinstance(tabel, dict)
+        assert isinstance(tabel_date, dict)
 
-        t = {}
+        for index_variabila in range(len(denumiri_variabile_numerice)):
+            valori = []
+            for siruta in tabel_date.keys():
+                # Extragem valorile pentru variabila curentă
+                valori.append(tabel_date[siruta][2 + index_variabila])
+
+            # Calculăm media
+            media_valori = sum(valori) / len(tabel_date)
+
+            # Calculăm deviația standard
+            suma_patrate_diferente = sum((val - media_valori) ** 2 for val in valori)
+            deviatia_standard = math.sqrt(suma_patrate_diferente / len(tabel_date))
+
+            # Calculăm coeficientul de variație
+            coeficient_variatie = deviatia_standard / media_valori if media_valori != 0 else 0
+
+            # Salvăm rezultatele în dicționar
+            rezultate_statistici[denumiri_variabile_numerice[index_variabila]] = (
+            media_valori, deviatia_standard, coeficient_variatie)
+
+        return rezultate_statistici
+
     except AssertionError:
-        return
+        return {}
+
+
+def functie_filtru(t, k):
+    if t[1][k] > 0:
+        return True
+    else:
+        return False
+
+
+def criteriu_sortare(t, k):
+    # t[0] - Siruta
+    # t[1] - tuplu de valori luate de variabile pentru localitatea cu Siruta t[0]
+    return t[1][k]
+
+
+def selector(t):
+    return (t[0], t[1][8:])
+
+
+def salvare(tabel, variabile, nume_instante="", nume_fisier="out.csv"):
+    fisier = open(nume_fisier, mode="w")
+    fisier.write(nume_instante + "," + ",".join(variabile) + "\n")
+    for v in tabel:
+        fisier.write(v + "," + ",".join([str(t) for t in tabel[v]])+"\n")
+    fisier.close()
--- 2_Seminar/pythonProject/seminar1.py
+++ 2_Seminar/pythonProject/seminar1.py
@@ -1,52 +1,60 @@
+import functii
 from functii import *
+from seminar2 import filename
 
-fisier = open("Educatie.csv")
-# print(type(fisier))
-linii = fisier.readlines()
-# print(linii)
-fisier.close()
-# Citire antet
-linia0 = linii[0][:-1].split(",")
-# print(linia0)
-nume_index = linia0[0]
-variabile = linia0[1:]
-variabile_numerice = variabile[2:]
-# print(nume_index, variabile, variabile_numerice, sep="\n")
-# Citire date
-tabel = dict()
-for i in range(1, len(linii)):
-    linie_str = linii[i][:-1].split(",")
-    linie = linie_str[1:3]
-    for j in range(3, len(linie_str)):
-        linie.append(float(linie_str[j]))
-    tabel[linie_str[0]] = tuple(linie)
+filename = "Educatie.csv"
+## Citire date din csv si salvare intr-un tabel (dictionar)
+tabel = functii.csv_to_dic(filename)
+print("Tabelul din CSV:")
 print(tabel)
+
+fisier2 = open("Educatie.csv")
+linii = fisier2.readlines()
+fisier2.close()
+
 # Cerinta 1
 cerinta1 = {}
 for v in tabel.keys():
     cerinta1[v] = (sum(tabel[v][2:6]), sum(tabel[v][6:]))
 print("--> Cerinta 1")
 print(cerinta1)
+
+# Cerinta 1.2
+
+linia0 = linii[0].strip().split(",")
+print("Linia 0:")
+print(linia0)
+nume_index = linia0[0]
+variabile = linia0[1:]
+variabile_numerice = variabile[2:]
+# print("Pregatire cerinta 2:")
+# print(nume_index, variabile, variabile_numerice, sep="\n")
+
 # Cerinta 2
+print("--> Valori numerice:")
+print(variabile_numerice)
+
 cerinta2 = calcul_indicatori(tabel, variabile_numerice)
+print("--> Cerinta 2:")
 for v in cerinta2:
     print(v, cerinta2[v])
-# Cerinta 3
-print("--> Cerinta 3")
-k = variabile_numerice.index("Abs_univ") + 2
-for v in filter(lambda x: functie_filtru(x, k), tabel.values()):
-    print(v)
 
-
-def convert_temp(temperature, unit):
-    if unit == 'C':
-        fahrenheit = (temperature*9/5) +32
-        return f"C {temperature}  is  F {fahrenheit} "
-    elif unit  == 'F':
-        celcius = (temperature-32) * 5/9
-        return f"the F {temperature} is the following amount in celcius: {celcius}"
-    else:
-        return "Invalid unit. Please enter 'C' for Celsius or 'F' for Fahrenheit."
-
-print("===========") 
-print(convert_temp(24,'C'))
+# # Cerinta 3
+# print("--> Cerinta 3")
+# k = variabile_numerice.index("Abs_univ") + 2
+# for v in filter(lambda x: functie_filtru(x, k), tabel.values()):
+#     print(v)
+#
+#
+# def convert_temp(temperature, unit):
+#     if unit == 'C':
+#         fahrenheit = (temperature*9/5) +32
+#         return f"C {temperature}  is  F {fahrenheit} "
+#     elif unit  == 'F':
+#         celcius = (temperature-32) * 5/9
+#         return f"the F {temperature} is the following amount in celcius: {celcius}"
+#     else:
+#         return "Invalid unit. Please enter 'C' for Celsius or 'F' for Fahrenheit."
+#
+# print("===========")
+# print(convert_temp(24,'C'))
