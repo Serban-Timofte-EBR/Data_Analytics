2a4cd97ba88dffbd8c67cd2b73eb5789fa6e96e6
serbantimofte <serban-timofte@outlook.com>
2024-11-09 21:02:08 +0200

    Seminarul 6

--- 6_seminar/pythonProject/main.py
+++ 6_seminar/pythonProject/main.py
@@ -122,14 +122,17 @@ for line in population:
     print(line)
 
 dict_localitati = csv_to_dict_localitati(filename_localitati)
+print("dict_localitati")
 for key, value in dict_localitati.items():
     print(key, value)
 
 dict_judete = csv_to_dict_judete(filename_judete)
+print("dict_judete")
 for key, value in dict_judete.items():
     print(key, value)
 
 dict_regiuni = csv_to_dict_regiuni(filename_regiuni)
+print("dict_regiuni")
 for key, value in dict_regiuni.items():
     print(key, value)
 
@@ -198,3 +201,121 @@ print("\nDistributia macroregiuni:")
 dict_pop_macroregiune = etnii_macroregiuni(population, dict_localitati, dict_judete, dict_regiuni)
 for key, value in dict_pop_macroregiune.items():
     print(key, value)
+
+# 2. Ponderi etnii
+def procente_etnii_localitati(population):
+    dict_localitati = {}
+    for pop in population:
+        total_pop = sum(pop[2:])
+        localitate = pop[1]
+
+        procente_etnii = []
+        for i in range(len(pop[2:])):
+            procente_etnii.append(round(pop[2+i] / total_pop, 2) * 100)
+
+        dict_localitati[localitate] = procente_etnii
+
+    return dict_localitati
+
+def procente_etnii_judete(dict_judete):
+    dict_procente_judete = {}
+    for key in dict_judete.keys():
+        values = dict_judete.get(key)
+        # print(values)
+        total_pop_jud = sum(values)
+        etnii_procente = [round(value / total_pop_jud,2) * 100 for value in values]
+        dict_procente_judete[key] = etnii_procente
+
+    return dict_procente_judete
+
+def procente_etnii_regiuni(dict_pop_regiuni):
+    dict_procente_regiuni = {}
+    for key in dict_pop_regiuni.keys():
+        values = dict_pop_regiuni.get(key)
+        total_pop = sum(values)
+        regiuni_procente = [round(value / total_pop,2) * 100 for value in values]
+
+        dict_procente_regiuni[key] = regiuni_procente
+
+    return dict_procente_regiuni
+
+etnii_localitati = procente_etnii_localitati(population)
+print("Distributia localitati procentuala:")
+for key, value in etnii_localitati.items():
+    print(key, value)
+
+etnii_judete = procente_etnii_judete(dict_pop_judete)
+print("Distributia judete procentuala:")
+for key, value in etnii_judete.items():
+    print(key, value)
+
+etnii_regiuni = procente_etnii_regiuni(dict_pop_regiuni)
+print("Distributia regiuni procentuala:")
+for key, value in etnii_regiuni.items():
+    print(key, value)
+
+# Indicele de disimilare
+    # n = numarul de localitati
+    # xi = populatie de o anumita etnie intr o localitate / judet
+    # Tx = totalul populatiei de respectiva etnie
+    # ri = restul populatiei dintr-o localitate / judet
+    # Tr = restul populatiei totale
+
+def calcul_populatie_totala(population):
+    total_pop = 0
+    for pop in population:
+        total_pop += sum(pop[2:])
+    return total_pop
+
+def calcul_populatie_etnie(population, index):
+    total = 0
+    for pop in population:
+        total += pop[index]
+    return total
+
+def calcul_populatie_etnie_judet(dict_pop_judet, index_etnie_pop, judet):
+    return dict_pop_judet[judet][index_etnie_pop]
+
+# print("Total population: " + str(calcul_populatie_totala(population)))
+# print("Total pop etnie 2 " + str(calcul_populatie_etnie(population, 3)))
+
+def calcul_indice_disimilare_judete(population, dict_pop_judet):
+    dict_indice_disimilare = {}
+
+    total_populatie = calcul_populatie_totala(population)
+
+    # Parcurgem fiecare județ
+    for judet, values_etnii in dict_pop_judet.items():
+        vector_indici = []  # Indicii pentru fiecare etnie
+
+        for etnie_index in range(len(values_etnii)):  # Iterăm pe fiecare etnie
+            xi = values_etnii[etnie_index]  # Populația etniei curente în județ
+            tx = calcul_populatie_etnie(population, etnie_index + 2)  # Total populație etnie
+            ri = sum(values_etnii) - xi  # Restul populației (în județ)
+            tr = total_populatie - tx  # Totalul restului populației (în țară)
+
+            # Calculăm indicele de disimilare pentru această etnie
+            suma_disimilare = 0
+            for pop in population:
+                if dict_localitati.get(pop[0])[1] == judet:  # Localitate din județul curent
+                    x_local = pop[2 + etnie_index]  # Populația etniei curente în localitate
+                    r_local = sum(pop[2:]) - x_local  # Restul populației în localitate
+                    if tx > 0 and tr > 0:  # Evităm divizarea la zero
+                        suma_disimilare += abs((x_local / tx) - (r_local / tr))
+
+            D = 0.5 * suma_disimilare
+            vector_indici.append(D)
+
+        # Adăugăm vectorul de indici pentru județul curent
+        dict_indice_disimilare[judet] = vector_indici
+
+    return dict_indice_disimilare
+
+dict_indici_disimilare = calcul_indice_disimilare_judete(population, dict_pop_judet)
+
+print("Indicii de disimilare pe județe:")
+for judet, indici in dict_indici_disimilare.items():
+    print(f"Județ: {judet}, Indicii: {indici}")
+
+
+
